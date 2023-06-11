3fd47a9a5376e13c62af97668da1e3bd7f39c610
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2023-06-12 00:25:04 +0530

    Create Floor in a Sorted Array.cpp

---
+++ Floor in a Sorted Array.cpp
@@ -0,0 +1,29 @@
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        
+        int start=0;
+        int end=n-1;
+        int res=-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(v[mid]==x){
+                return mid;
+            }
+            else if(v[mid]<x){
+                res=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return res;
+        
+    }
+};
