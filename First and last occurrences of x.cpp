3d026e7b54bce16ad9a9a32a2b48e58d5a3e2358
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2023-06-12 14:44:51 +0530

    Create First and last occurrences of x.cpp

---
+++ First and last occurrences of x.cpp
@@ -0,0 +1,15 @@
+vector<int> find(int arr[], int n , int x )
+while (start <= end) {
+    int mid = start + (end - start) / 2;
+    if (v[mid] == key) {
+      res = mid;
+      start = mid + 1;
+
+    } else if (key < v[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return res;
+}
