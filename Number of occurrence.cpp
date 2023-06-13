85b3c90d8d4f944dcfd64e4ae6b5fd3262ed24a3
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2023-06-13 13:00:33 +0530

    Update Number of occurrence.cpp

--- Number of occurrence.cpp
+++ Number of occurrence.cpp
@@ -1,1 +1,26 @@
 
+class Solution{
+public:	
+	/* if x is present in arr[] then returns the count
+		of occurrences of x, otherwise returns 0. */
+	int count(int arr[], int n, int x) {
+	    // code here
+	    int start=0;
+	    int end=n-1;
+	    int ans=0;
+	    while(start<=end){
+	        int mid=start+(end-start)/2;
+	        if(arr[mid]==x){
+	            ans++;
+	            end=mid-1;
+	        }
+	        else if(arr[mid]>x){
+	            end=mid-1;
+	        }
+	        else{
+	            start=mid+1;
+	        }
+	    }
+	    return ans;
+	}
+};
