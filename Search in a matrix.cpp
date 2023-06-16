c63650892b4a13e51165a361851f113ec4c6d0ef
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2023-06-17 00:23:57 +0530

    Create Search in a matrix.cpp

---
+++ Search in a matrix.cpp
@@ -0,0 +1,23 @@
+class Solution{
+public:	
+	int matSearch (vector <vector <int>> &mat, int N, int M, int X)
+	{
+	    // your code here
+	    int i=0;
+	    int j=M-1;
+	    while(i<N && j>=0){
+	        if(mat[i][j]==X){
+	            
+	            return 1;
+	            
+	        }
+	        if (mat[i][j]>X){
+	            j--;
+	        }
+	        else{
+	            i++;
+	        }
+	    }
+	    return 0;
+	}
+};
