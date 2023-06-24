1aab897bd7ffeb6c7c6411fcbec3ca879c1a8254
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2023-06-24 23:54:22 +0530

    Update Koko Eating Bananas.cpp

--- Koko Eating Bananas.cpp
+++ Koko Eating Bananas.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+  public:
+    int Solve(int N, vector<int>& piles, int H) {
+        int s=1, e=1000000000; //e is h of piles[i] given in constraint
+
+        int mid= s+(e-s)/2;
+
+        while(s<e){
+
+             mid = s+(e-s)/2; 
+
+        int time=0;
+
+        for(int p: piles) time+=(p-1)/mid+1;
+
+        if(time<=H) e=mid;
+
+             else s=mid+1;
+
+        }
+
+        return s;
+    }
+};
