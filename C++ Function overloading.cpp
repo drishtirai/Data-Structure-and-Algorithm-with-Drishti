4f12ae2d44f858b2540ec1acd2d873d69a98a62a
Drishti Rai <76700390+drishtirai@users.noreply.github.com>
2023-07-06 23:49:17 +0530

    Create C++ Function overloading.cpp

---
+++ C++ Function overloading.cpp
@@ -0,0 +1,20 @@
+void volume(int s)
+{
+    // Add your code here.
+       cout<<s * s* s<<endl;
+
+}
+
+void volume(int r, int h)
+{
+    // Add your code here.
+       cout<<3.14159 * r * r * h<<endl; 
+
+}
+
+void volume(int l, int b, int h)
+{
+    // Add your code here.
+    cout<<l * b * h<<endl;
+}
+
